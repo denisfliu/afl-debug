03a0dcded5c6d79480977f384dc0f6a59b88dcf7
denisfliu <denisliu10@gmail.com>
2023-10-12 21:00:18 -0700

    new compare file

---
+++ compare.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import glob
+
+PATH = 'default/queue'
+FILETEMPLATE= 'id:'
+
+dir1 = sys.argv[1]
+dir2 = sys.argv[2]
+
+def getfile(directory, number):
+    files = glob.glob(f"{directory}{number:06}*")
+    if len(files) == 0:
+        return None
+    elif len(files) > 1:
+        print("Something is wrong, {files}")
+        return None
+    else:
+        return files[0]
+
+def same_content(file1, file2):
+    with open(file1, 'rb') as f:
+        content1 = f.read()
+    with open(file2, 'rb') as f:
+        content2 = f.read()
+    if content1 == content2:
+        return True
+    else:
+        return False
+
+
+def compare(dir1, dir2):
+    d1 = os.path.join(dir1, PATH)
+    d2 = os.path.join(dir2, PATH)
+    d1_len = len([entry for entry in os.listdir(d1) if os.path.isfile(os.path.join(d1, entry))])
+    d2_len = len([entry for entry in os.listdir(d2) if os.path.isfile(os.path.join(d2, entry))])
+    print(f'{d1} | Length: {d1_len}', f'{d2} | Length: {d2_len}', sep='\n')
+
+    d1 = os.path.join(dir1, PATH, FILETEMPLATE)
+    d2 = os.path.join(dir2, PATH, FILETEMPLATE)
+
+    i = 0
+    unidentical_count = 0
+    unidentical_files = []
+    first_files = []
+    f1 = getfile(d1, i)
+    f2 = getfile(d2, i)
+    while (f1 is not None and f2 is not None) or i == 0:
+        if i < 5:
+            first_files.append((f1, f2))
+
+        if not same_content(f1, f2):
+            unidentical_files.append((f1, f2))
+            unidentical_count += 1
+        # else:
+        #     print(f'{i:06}, Same')
+        i += 1
+        f1 = getfile(d1, i)
+        f2 = getfile(d2, i)
+    return unidentical_count / i, unidentical_count, i, unidentical_files, first_files
+
+def print_files(files, prefix):
+    for i, (f1, f2) in enumerate(files):
+        print(f'{prefix} {i}:\n{f1}\n{f2}\n')
+
+
+def main():
+    percentage, unidentical, total, files, first_files = compare(dir1, dir2)
+    print(f'{unidentical} files differ out of {total} files\n')
+    #print_files(first_files, 'File')
+    #if percentage < .05:
+    #    print_files(files, 'Difference')
+    #print(f'{percentage * 100} percent of files differ')
+
+
+if __name__ == "__main__":
+    main()
