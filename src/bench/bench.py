39cdcef6c4657b09820c2fbbd3a60acc4b7b8d1e
yangbranden <byang64@asu.edu>
2023-10-23 20:17:33 -0700

    added clearer output for program logic

--- src/bench/bench.py
+++ src/bench/bench.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
 import os
+import sys
 
 from dataclasses import dataclass
 from enum import Enum
@@ -26,7 +27,12 @@ class Bench:
     def bench(self):
         base_path = os.path.join(self.config.fuzz.fuzz_folder, self.config.bench.base_folder_path)
         output_path = os.path.join(self.config.fuzz.fuzz_folder, "outputs")
+        inputs_path = os.path.join(self.config.fuzz.fuzz_folder, self.config.fuzz.inputs)
         assert not os.path.exists(os.path.join(output_path, "bench0")), f"Existing benchmark found at {output_path}."
+        assert os.path.exists(inputs_path), f"No valid seeds found at {inputs_path}. Please verify your inputs."
+        assert not os.listdir(inputs_path), f"No valid seeds found at {inputs_path}. Please verify your inputs."
+        if not os.path.exists(output_path):
+            subprocess.run(f"mkdir {output_path}".split())
 
         for i in range(self.iterations):
             fuzz_path = os.path.join(output_path, f"outputs/bench{i}")
@@ -35,11 +41,15 @@ class Bench:
 
     # TODO: change this so it's more modular (we will need to implement different exec_fuzz functions for different fuzzers)
     def exec_fuzz(self, output_dir):
-        fancy_print(f"Starting fuzzing process for {output_dir}...")
+        fancy_print(f"Starting fuzzing process for {self.config.afl_path}...")
         try:
-            subprocess.run(f"{self.config.afl_path} -i {self.config.fuzz.inputs} -o {output_dir} -- {self.custom_binary_dir} @@".split(), timeout=self.time+2) # 2 seconds for startup
+            if self.custom_binary_dir is not None:
+                subprocess.run(f"sudo {self.config.afl_path} -i {self.config.fuzz.inputs} -o {output_dir} -- {self.custom_binary_dir} @@".split(), timeout=self.time+2) # 2 seconds for startup
+            else:
+                binary_path = os.path.join(self.config.fuzz.fuzz_folder, self.config.fuzz.binary)
+                subprocess.run(f"sudo {self.config.afl_path} -i {self.config.fuzz.inputs} -o {output_dir} -- {binary_path} @@".split(), timeout=self.time+2) # 2 seconds for startup
         except subprocess.TimeoutExpired:
-            fancy_print(f"Completed fuzzing process for {output_dir}.")
+            fancy_print(f"Completed fuzzing process for {self.config.afl_path}.\nResults in: {output_dir}")
 
 def main(args):
     config = get_config()
@@ -49,6 +59,11 @@ def main(args):
     
 
 if __name__ == "__main__":
+    if len(sys.argv) != 7:
+        print("Usage: python -m src.bench.bench -b [# of binary] -t [time] -i [iterations]")
+        print("Binaries supported:")
+        sys.exit(1)
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--binary_type", type=int, required=True)
     parser.add_argument("-c", "--custom_binary_dir", type=str, required=False)
