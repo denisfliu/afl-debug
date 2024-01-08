c7e0039c3c4edbdf40f4bb68f6252faf34446bbe
denisfliu <denisliu10@gmail.com>
2024-01-08 13:02:30 -0700

    modular running

--- src/components/replay.c
+++ src/components/replay.c
@@ -57,7 +57,7 @@
         int32_t _len = (int32_t)(len);               \
         int32_t _res = read(fd, buf, _len);          \
         if (_res != _len)                            \
-            RPFATAL(_res, "Short read from %s", fn); \
+            printf("Short read from %s", fn); \
                                                      \
     } while (0)
 
